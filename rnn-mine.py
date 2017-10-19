5d0018f5c247d778105f3cf603f2f4ad6bb302ed
Phillip Miguel dos Passos <phillipmigueldospassos@gmail.com>
2017-10-19 21:48:50 -0200

    update

---
+++ rnn-mine.py
@@ -0,0 +1,141 @@
+#Import Libraries
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+import shutil
+import tensorflow.contrib.learn as tflearn
+import tensorflow.contrib.layers as tflayers
+from tensorflow.contrib.learn.python.learn import learn_runner
+import tensorflow.contrib.metrics as metrics
+import tensorflow.contrib.rnn as rnn
+
+#random.seed(111)
+#rng = pd.date_range(start='2000', periods=209, freq='M')
+#ts = pd.Series(np.random.uniform(-10, 10, size=len(rng)), rng).cumsum()
+#ts.plot(c='b', title='Example Time Series')
+#plt.show()
+#ts.head(10)
+# Convert data into array that can be broken up into training "batches" 
+# that we will feed into our RNN model. Note the shape of the arrays.
+num_periods = 1000
+
+x = np.linspace(-np.pi, np.pi, 1001)
+"""
+plt.plot(x, np.sin(x))
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
+plt.axis('tight')
+plt.show()
+"""
+#print(x)
+TS = np.array(np.sin(x))
+lp = np.linspace(np.pi, (np.pi*14), num_periods)
+forecast = np.array(np.sin(lp)).reshape(-1, num_periods, 1)
+"""
+plt.plot(lp, np.ravel(forecast))
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
+plt.axis('tight')
+plt.show()
+"""
+
+num_periods = num_periods
+f_horizon = 1  #forecast horizon, one period into the future
+
+x_data = TS[:(len(TS)-(len(TS) % num_periods))]
+x_batches = x_data.reshape(-1, num_periods, 1)
+
+y_data = TS[1:(len(TS)-(len(TS) % num_periods))+f_horizon]
+y_batches = y_data.reshape(-1, num_periods, 1)
+#print (len(x_batches))
+#print (x_batches.shape)
+#print (x_batches[0:2])
+
+#print (y_batches[0:1])
+#print (y_batches.shape)
+
+# Pull out our test data
+
+def test_data(series,forecast,num_periods):
+    test_x_setup = TS[-(num_periods + forecast):]
+    testX = test_x_setup[:num_periods].reshape(-1, num_periods, 1)
+    testY = TS[-(num_periods):].reshape(-1, num_periods, 1)
+    return testX,testY
+
+X_test, Y_test = test_data(TS,f_horizon,num_periods )
+#print (X_test.shape)
+#print (X_test)
+
+
+
+
+
+
+tf.reset_default_graph()   #We didn't have any previous graph objects running, but this would reset the graphs
+
+num_periods = num_periods      #number of periods per vector we are using to predict one period ahead
+inputs = 1            #number of vectors submitted
+#100
+hidden = 100         #number of neurons we will recursively work through, can be changed to improve accuracy
+output = 1            #number of output vectors
+
+X = tf.placeholder(tf.float32, [None, num_periods, inputs])   #create variable objects
+y = tf.placeholder(tf.float32, [None, num_periods, output])
+
+
+basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=hidden, activation=tf.nn.relu)   #create our RNN object
+rnn_output, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32)               #choose dynamic over static
+
+learning_rate = 0.00001   #small learning rate so we don't overshoot the minimum
+
+stacked_rnn_output = tf.reshape(rnn_output, [-1, hidden])           #change the form into a tensor
+stacked_outputs = tf.layers.dense(stacked_rnn_output, output)        #specify the type of layer (dense)
+outputs = tf.reshape(stacked_outputs, [-1, num_periods, output])          #shape of results
+ 
+loss = tf.reduce_sum(tf.square(outputs - y))    #define the cost function which evaluates the quality of our model
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)          #gradient descent method
+training_op = optimizer.minimize(loss)          #train the result of the application of the cost_function                                 
+
+init = tf.global_variables_initializer()           #initialize all the variables
+
+
+
+
+
+# 10000
+epochs = 100000     #number of iterations or training cycles, includes both the FeedFoward and Backpropogation
+
+with tf.Session() as sess:
+    init.run()
+    for ep in range(epochs):
+        sess.run(training_op, feed_dict={X: x_batches, y: y_batches})
+        if ep % 100 == 0:
+            mse = loss.eval(feed_dict={X: x_batches, y: y_batches})
+            print(ep, "\tMSE:", mse)
+    
+    #y_pred = sess.run(outputs, feed_dict={X: X_test})
+    z_pred = sess.run(outputs, feed_dict={X: forecast})
+
+
+# plot
+"""
+plt.title("Forecast vs Actual", fontsize=14)
+plt.plot(pd.Series(np.ravel(Y_test)), "bo", markersize=1, label="Actual")
+#plt.plot(pd.Series(np.ravel(Y_test)), "w*", markersize=1)
+plt.plot(pd.Series(np.ravel(y_pred)), "r.", markersize=1, label="Forecast")
+plt.legend(loc="upper left")
+plt.xlabel("Time Periods")
+
+plt.show()
+"""
+plt.plot(lp, pd.Series(np.ravel(forecast)), "g", markersize=1, label="Actual")
+plt.plot(lp, pd.Series(np.ravel(z_pred)), "y", markersize=1, label="Forecast")
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
+plt.axis('tight')
+plt.show()
